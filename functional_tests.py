9243eae89d016147df79936dcde22ea4dbcb64f8
drairsong <2097462337@qq.com>
2020-04-04 20:06:49 +0800

    First unit test and url mapping, dummy view"

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):   #(1)
+    
+    def setUp(self):    #(3)
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self): #(3)
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):#(2)
+
+
+        self.browser.get('http://loaclhost:8000')
+
+        self.assertIn('To-Do',self.browser.title)   #(4)
+        self.fail('Finish the test!')   #(5)
+
+if __name__ == '__main__':  #(6)
+    unittest.main(warnings='ignore')    #(7)
+
+
+
