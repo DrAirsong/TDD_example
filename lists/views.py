352eef1a791bb82c2eb1cf5954d2bc5f91d22f70
drairsong <2097462337@qq.com>
2020-06-19 21:43:39 +0800

    set STATIC_ROOT in settings and disable admin

--- lists/views.py
+++ lists/views.py
@@ -4,22 +4,17 @@ from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
-    #if request.method == 'POST':
-    #    Item.objects.create(text=request.POST['item_text'])
-    #    return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id= list_id)
-    return render(request, 'list.html', {'list':list_})
-
-    #items = Item.objects.filter(list=list_)
-    #return render(request, 'list.html', {'items':items})
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+def view_list(request, list_id):
+    list_ = List.objects.get(id= list_id)
+    items = Item.objects.filter(list=list_)
+    return render(request, 'list.html', {'list':list_})
     
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
